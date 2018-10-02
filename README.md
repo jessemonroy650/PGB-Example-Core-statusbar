4c9862d940a15af88c41be4883fa094becec9657
jessemonroy650 <jessemonroy650@yahoo.com>
2018-10-02 14:50:21 -0600

    initial entry

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PGB-Example-Core-Statusbar
+
 PGB Example Core using the Cordova 'Statusbar' plugin
+
+* Statusbar - https://www.npmjs.com/package/cordova-plugin-statusbar
---
+++ statusbar.js
@@ -0,0 +1,11 @@
+var statusbarPlugin = {
+    version : '1.0.0',
+
+    //
+    properties2HTML : function (outputElementId) {
+        var statusbarStuff = "";
+
+        document.getElementById(outputElementId).innerHTML = statusbarStuff;
+    },
+    //
+}
